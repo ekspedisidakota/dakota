3d51a339dae5a58268e1e543e4fedb296bc24c75
alldie1207 <dieabra@gmail.com>
2018-02-04 11:24:14 +0700

    add funcion model kelurahan

--- application/models/adminm.php
+++ application/models/adminm.php
@@ -2,8 +2,8 @@
 
 class adminm extends CI_Model{
 
-// ===========================================          Script         ======================================= //
-// ===========================================                         ======================================= //
+// =========================================================================================================== //
+// =========================================================================================================== //
 
 
     public function getAllData($table)
@@ -33,14 +33,14 @@ class adminm extends CI_Model{
 
     public function viewByProvinsi($propinsiid){
     $this->db->where('propinsiid', $propinsiid);
-    $result = $this->db->get('tbl_propinsi_kota')->result(); // Tampilkan semua data kota berdasarkan id provinsi
+    $result = $this->db->get('tbl_propinsi_kota')->result();
     
     return $result; 
   }
 
 
-// ===========================================          Script         ======================================= //
-// ===========================================                         ======================================= //
+// =========================================================================================================== //
+// =========================================================================================================== //
     function get_all_data_prov(){
         return $this->db->query("
             SELECT *, a.nama, a.propinsiid,
@@ -51,8 +51,8 @@ class adminm extends CI_Model{
         ")->result();
     }
 
-// ===========================================          Script         ======================================= //
-// ===========================================                         ======================================= //
+// =========================================================================================================== //
+// =========================================================================================================== //
     function get_all_data_kota($id_provinsi){
         return $this->db->query("
             SELECT *
@@ -60,8 +60,9 @@ class adminm extends CI_Model{
             WHERE a.propinsiid = '$id_provinsi'
         ")->result();
     }
-// ===========================================          Script         ======================================= //
-// ===========================================                         ======================================= //
+
+// =========================================================================================================== //
+// =========================================================================================================== //
     function get_all_data_kecamatan(){
         return $this->db->query("
             SELECT *, a.nama, a.propinsiid,
@@ -71,37 +72,41 @@ class adminm extends CI_Model{
         ")->result();
     }
 
-// ===========================================          Script         ======================================= //
-// ===========================================                         ======================================= //
+// =========================================================================================================== //
+// =========================================================================================================== //
+    function get_all_data_kelurahan(){
+        return $this->db->query("
+            SELECT *, a.nama, a.propinsiid,
+            (select count(propinsiid) as jum from tbl_propinsi_kota where propinsiid=a.propinsiid) as jumlah_kota
+            FROM tbl_propinsi a
+            LEFT JOIN tbl_propinsi_kota b ON a.propinsiid = b.propinsiid
+        ")->result();
+    }
+// =========================================================================================================== //
+// =========================================================================================================== //
 
     function login($username, $password) {
-        //create query to connect user login database
         $this->db->select('*');
         $this->db->from('tbl_user');
-/*
-        $this->db->join('tbl_pegawai', 'tbl_user.id_pegawai = tbl_pegawai.id_pegawai');
-*/
         $this->db->where('username', $username);
         $this->db->where('password', md5($password));
         $this->db->limit(1);
 
-        //get query and processing
         $query = $this->db->get();
         if($query->num_rows() == 1) {
-            return $query->result(); //if data is true
+            return $query->result();
         } else {
-            return false; //if data is wrong
+            return false;
         }
     }
 
-/*
-Source Code by : Aldy Muldani
-Email : dieabra@gmail.com
-Github : https://github.com/alldie1207
-Line : alldie1207
-No: 082295673583
-2017-2018
-*/// ===========================================          Script         ======================================= //
+// Source Code by : Aldy Muldani
+// Email : dieabra@gmail.com
+// Github : https://github.com/alldie1207
+// Line : alldie1207
+// No: 082295673583
+
+// ===========================================          Script         ======================================= //
 // ===========================================                         ======================================= //
 
 }
